21de7651d015d6e1fec9ad68ba26ea9a10b87b4a
CryptoWingnut <cryptowingnut@gmail.com>
2024-02-14 16:09:38 -0400

    try and use tagged username in command handlers

--- StackerBot/DiscordCommandsModule.cs
+++ StackerBot/DiscordCommandsModule.cs
@@ -8,33 +8,39 @@ public sealed class DiscordCommandsModule(IExternals externals, IRepository repo
   [RequireRoles(RoleCheckMode.Any, Parameters.REQUIRED_ROLE)]
   public async Task AddYouTubeSubscription(CommandContext context, string channelName) {
     try {
-      var existingResult = await repository.FindYouTubeSubscription(channelName, CancellationToken.None);
+      var channelNameActual = channelName;
+
+      if (string.IsNullOrEmpty(channelName) && context.Message.MentionedUsers.Count > 0) {
+        channelNameActual = $"@{context.Message.MentionedUsers[0].Username}";
+      }
+
+      var existingResult = await repository.FindYouTubeSubscription(channelNameActual, CancellationToken.None);
       if (existingResult.IsType(typeof(YouTubeSubscriptionModel))) {
-        await context.RespondAsync($"Already subscribed to: {channelName}");
+        await context.RespondAsync($"Already subscribed to: {channelNameActual}");
         return;
       }
 
-      var channelId = await externals.GetYouTubeChannelId(channelName);
+      var channelId = await externals.GetYouTubeChannelId(channelNameActual);
 
       if (channelId == null) {
-        await context.RespondAsync($"Failed to find channel: {channelName}");
+        await context.RespondAsync($"Failed to find channel: {channelNameActual}");
         return;
       }
 
       var success = await externals.SubscribeToYouTubeChannel(channelId);
 
       if (!success) {
-        await context.RespondAsync($"Failed to subscribe to: {channelName}");
+        await context.RespondAsync($"Failed to subscribe to: {channelNameActual}");
         return;
       }
 
       var subscription = new YouTubeSubscriptionModel {
-        Id = Guid.NewGuid(), ChannelId = channelId, ChannelName = channelName, AddedAt = DateTime.UtcNow,
+        Id = Guid.NewGuid(), ChannelId = channelId, ChannelName = channelNameActual, AddedAt = DateTime.UtcNow,
         AddedBy = context.User.Id, LastVideo = DateTime.UtcNow
       };
 
       await repository.AddYouTubeSubscription(subscription, CancellationToken.None);
-      await context.RespondAsync($"Subscribed to: {channelName}");
+      await context.RespondAsync($"Subscribed to: {channelNameActual}");
     } catch (Exception error) {
       logger.LogError(error, "Exception occured while adding YouTube subscription");
       await context.RespondAsync("Error! Please notify Wingnut!");
@@ -45,9 +51,15 @@ public sealed class DiscordCommandsModule(IExternals externals, IRepository repo
   [RequireRoles(RoleCheckMode.Any, Parameters.REQUIRED_ROLE)]
   public async Task RemoveYouTubeSubscription(CommandContext context, string channelName) {
     try {
-      var existingResult = await repository.FindYouTubeSubscription(channelName, CancellationToken.None);
+      var channelNameActual = channelName;
+
+      if (string.IsNullOrEmpty(channelName) && context.Message.MentionedUsers.Count > 0) {
+        channelNameActual = $"@{context.Message.MentionedUsers[0].Username}";
+      }
+
+      var existingResult = await repository.FindYouTubeSubscription(channelNameActual, CancellationToken.None);
       if (existingResult.IsType(typeof(NotFound))) {
-        await context.RespondAsync($"Not subscribed to: {channelName}");
+        await context.RespondAsync($"Not subscribed to: {channelNameActual}");
         return;
       }
 
@@ -55,12 +67,12 @@ public sealed class DiscordCommandsModule(IExternals externals, IRepository repo
       var success = await externals.UnSubscribeFromYouTubeChannel(channel.ChannelId);
 
       if (!success) {
-        await context.RespondAsync($"Failed to unsubscribe from: {channelName}");
+        await context.RespondAsync($"Failed to unsubscribe from: {channelNameActual}");
         return;
       }
 
       await repository.RemoveYouTubeSubscription(channel.Id, CancellationToken.None);
-      await context.RespondAsync($"Unsubscribed from: {channelName}");
+      await context.RespondAsync($"Unsubscribed from: {channelNameActual}");
     } catch (Exception error) {
       logger.LogError(error, "Exception occured while removing YouTube subscription");
       await context.RespondAsync("Error! Please notify Wingnut!");
