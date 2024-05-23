0e0c7ec7e10f46aa836c510964aae67119c602b7
CryptoWingnut <cryptowingnut@gmail.com>
2024-05-23 20:48:29 -0300

    added member list command

--- StackerBot/DiscordCommandsModule.cs
+++ StackerBot/DiscordCommandsModule.cs
@@ -1,12 +1,52 @@
 ﻿using System.Text;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 using StackerBot.Services;
 
 namespace StackerBot;
 
 public sealed class DiscordCommandsModule(IExternals externals, IRepository repository, ILogger<DiscordCommandsModule> logger, EventBus eventBus) : BaseCommandModule {
   private static DateTime _inviteLeaderboardAntispam = DateTime.MinValue;
+
+  [Command("member-list")]
+  [RequireRoles(RoleCheckMode.Any, Parameters.REQUIRED_ROLE)]
+  public async Task GetMemberList(CommandContext context, string email) {
+    try {
+      var members = await context.Guild.GetAllMembersAsync();
+      var list = new StringBuilder();
+
+      foreach (var member in members) {
+        if (member.IsBot) {
+          continue;
+        }
+
+        list.AppendLine(member.DisplayName);
+      }
+
+      var message = new MimeMessage();
+      message.From.Add(new MailboxAddress("SSBot", "stackerbot0@gmail.com"));
+      message.To.Add(new MailboxAddress("", email));
+      message.Subject = "Stacker Social Member List";
+
+      message.Body = new TextPart("plain") {
+        Text = list.ToString()
+      };
+
+      using var client = new SmtpClient();
+      await client.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+      await client.AuthenticateAsync("stackerbot0@gmail.com", Environment.GetEnvironmentVariable("EMAIL_PASSWORD"));
+      await client.SendAsync(message);
+      await client.DisconnectAsync(true);
+
+      await context.RespondAsync("Member list has been emailed");
+    } catch (Exception error) {
+      logger.LogError(error, "Exception occured while generating member list");
+      await context.RespondAsync("Error! Please notify Wingnut!");
+    }
+  }
 
   [Command("invites")]
   public async Task GetInviteLeaderboard(CommandContext context) {
